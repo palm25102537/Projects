da49d9e19b87d8ff9c2cd784a443b612af301f28
Wasin <xelticross@gmail.com>
2022-06-19 10:00:35 +0700

    learn DOM

--- JS-Projects/GuessNumber/script.js
+++ JS-Projects/GuessNumber/script.js
@@ -1,3 +1,12 @@
 'use strict';
 
 console.log(document.querySelector('.message').innerHTML);
+
+setTimeout(() => {
+  document.querySelector('.message').textContent = 'Correct Number';
+  document.querySelector('.number').textContent = 12;
+  document.querySelector('.guess').value = 12;
+  document.querySelector('.score').textContent = 20;
+}, 1000);
+
+console.log(document.querySelector('.guess').value);
